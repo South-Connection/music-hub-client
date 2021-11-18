bdfb19b4a8e86a7811221101cbd52f1bd36ef810
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-11-18 11:14:38 +0100

    Display songs & link to youtube working

--- src/components/playlists/PlaylistDetails.js
+++ src/components/playlists/PlaylistDetails.js
@@ -1,74 +1,86 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link, Redirect } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Link, Redirect } from "react-router-dom";
 
 class PlaylistDetails extends Component {
-  state = {}
+  state = {};
 
-
-    componentDidMount(){
+  componentDidMount() {
     this.getSinglePlaylist();
-    }
+  }
 
-    getSinglePlaylist = () => {
+  getSinglePlaylist = () => {
     const { params } = this.props.match;
-    axios.get(`${process.env.REACT_APP_API_URL}/playlists/${params.id}`)
-    .then( responseFromApi =>{
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/playlists/${params.id}`)
+      .then((responseFromApi) => {
         const thePlaylist = responseFromApi.data;
         this.setState(thePlaylist);
-    })
-    .catch((err)=>{
-        console.log(err)
-    }, {withCredentials: true })
-    }
-    // DELETE PROJECT:
-    deleteProject = () => {
-        const { params } = this.props.match;
-        console.log('hello clg', params)
-        axios.delete(`${process.env.REACT_APP_API_URL}/playlists/${params.id}`)
-        .then( () =>{
-            this.props.history.push('/playlists');        
-        })
-        .catch((err)=>{
-            console.log(err)
-        }, {withCredentials: true })
-    }
+      })
+      .catch(
+        (err) => {
+          console.log(err);
+        },
+        { withCredentials: true }
+      );
+  };
+  // DELETE PROJECT:
+  deleteProject = () => {
+    const { params } = this.props.match;
+    console.log("hello clg", params);
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/playlists/${params.id}`)
+      .then(() => {
+        this.props.history.push("/playlists");
+      })
+      .catch(
+        (err) => {
+          console.log(err);
+        },
+        { withCredentials: true }
+      );
+  };
 
-  //   renderEditForm = () => {
-  //     if(this.state.title){
-  //       return <EditPlaylist thePlaylist={this.state} getThePlaylist={this.getSinglePlaylist} />
-  //     } 
-  // } 
-
-    render(){
-    return(
+  render() {
+    
+    return (
       <div>
-      {/* <Navbar userData={this.state.user} userIsLoggedIn={this.state.isLoggedIn} getUser={this.getTheUser} /> */}
-        <h1>{this.state.title}</h1>
-        <p>{this.state.description}</p>
+        {this.state.songs === undefined ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <h1>{this.state.title}</h1>
+            <p>{this.state.description}</p>
+
+            <br />
+
+            <div>
+              {this.state.songs.map((elm) => {
+                return (
+                  <div key={elm._id}>
+                    <Link to={{ pathname: `${elm.link}` }} target="_blank" >{elm.title}</Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          
+        )}
+        <br />
+
         <Link to={`/playlists/${this.state._id}/edit`}>Edit playlist</Link>
-        <br/>
-        {/* { this.state.songs.map( song => {
-            return (
-              <ol key={song._id}>                
-                  <li>{song.title}</li>
-                  <button>delete</button>
-              </ol>
-            )})
-          } // Work in progress*/} 
+        <br />
+
         <button type="button">Add Song</button>
 
-        <br/>
+        <br />
         <button onClick={() => this.deleteProject()}>Delete playlist</button>
-        <br/>
-        
-        <Link to={'/playlists'}>Back to playlists</Link>
-        <br/>
-        
+        <br />
+
+        <Link to={"/playlists"}>Back to playlists</Link>
+        <br />
       </div>
-    )
+    );
   }
 }
 
